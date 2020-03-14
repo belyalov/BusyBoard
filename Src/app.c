27d8db6958d1a47131c8c3b87c5ac132ff742d5a
Konstantin Belyalov <arsenicus@mail.ru>
2020-03-13 21:54:25 -0700

    Temporary disable interrupts / test 12v output

--- Src/app.c
+++ Src/app.c
@@ -6,10 +6,15 @@
 void app()
 {
   // Init configuration //
-  DEBUGLN("\nLoRa LED Strip Controller started.");
+  DEBUGLN("\nBusyBoard started!");
 
   while (1) {
-    HAL_Delay(1000);
+    DEBUGLN("12v on");
+    HAL_GPIO_WritePin(EN_12V_GPIO_Port, EN_12V_Pin, GPIO_PIN_SET);
+    HAL_Delay(5000);
+    DEBUGLN("12v off");
+    HAL_GPIO_WritePin(EN_12V_GPIO_Port, EN_12V_Pin, GPIO_PIN_RESET);
+    HAL_Delay(5000);
   }
 }
 
--- Src/main.c
+++ Src/main.c
@@ -359,25 +359,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(BATT_PWR_GPIO_Port, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-
 }
 
 /* USER CODE BEGIN 4 */
